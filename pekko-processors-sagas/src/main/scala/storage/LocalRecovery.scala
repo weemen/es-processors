4b9acad597173725284afe0493eafcc25414e5e0
Leon Weemen <weemen@leonweemen.nl>
2026-03-13 19:18:19 +0100

    Logging added

--- pekko-processors-sagas/src/main/scala/storage/LocalRecovery.scala
+++ pekko-processors-sagas/src/main/scala/storage/LocalRecovery.scala
@@ -2,12 +2,15 @@ package storage
 
 import actors.CborSerializable
 import config.StorageConfig
+import org.slf4j.LoggerFactory
 
 import java.io.*
 
 class LocalRecovery(config: StorageConfig.Local) extends Recovery {
+  private val logger = LoggerFactory.getLogger(getClass)
 
   override def recover(actorId: String): List[CborSerializable] = {
+    logger.info(s"[LocalRecovery] Recovering events for actorId: $actorId from path: ${config.storagePath}")
     var events      = List.empty[CborSerializable]
     var eventNumber = 0
     var keepGoing   = true
@@ -15,6 +18,7 @@ class LocalRecovery(config: StorageConfig.Local) extends Recovery {
     while (keepGoing) {
       val file = new File(s"${config.storagePath}/$actorId-$eventNumber.bin")
       if (file.exists()) {
+        logger.debug(s"[LocalRecovery] Found recovery file: ${file.getPath}")
         val ois   = new ObjectInputStream(new FileInputStream(file))
         val event = ois.readObject().asInstanceOf[CborSerializable]
         events = events :+ event
@@ -24,6 +28,7 @@ class LocalRecovery(config: StorageConfig.Local) extends Recovery {
         keepGoing = false
       }
     }
+    logger.info(s"[LocalRecovery] Recovered ${events.size} events for actorId: $actorId")
     events
   }
 
@@ -32,15 +37,19 @@ class LocalRecovery(config: StorageConfig.Local) extends Recovery {
     while (new File(s"${config.storagePath}/$actorId-$eventNumber.bin").exists()) {
       eventNumber += 1
     }
-    val oos         = new ObjectOutputStream(new FileOutputStream(s"${config.storagePath}/$actorId-$eventNumber.bin"))
+    val fileName = s"${config.storagePath}/$actorId-$eventNumber.bin"
+    logger.info(s"[LocalRecovery] Saving event for actorId: $actorId to file: $fileName")
+    val oos      = new ObjectOutputStream(new FileOutputStream(fileName))
     oos.writeObject(event)
     oos.close()
   }
 
   override def delete(actorId: String): Unit = {
+    logger.info(s"[LocalRecovery] Deleting all recovery files for actorId: $actorId")
     var eventNumber = 0
     var file        = new File(s"${config.storagePath}/$actorId-$eventNumber.bin")
     while (file.exists()) {
+      logger.debug(s"[LocalRecovery] Deleting file: ${file.getPath}")
       file.delete()
       eventNumber += 1
       file = new File(s"${config.storagePath}/$actorId-$eventNumber.bin")
--- pekko-processors-sagas/src/main/scala/storage/RedisRecovery.scala
+++ pekko-processors-sagas/src/main/scala/storage/RedisRecovery.scala
@@ -2,13 +2,18 @@ package storage
 
 import actors.CborSerializable
 import config.StorageConfig
+import org.slf4j.LoggerFactory
 import redis.clients.jedis.Jedis
+
 import java.io.*
 import java.util.Base64
 
 class RedisRecovery(config: StorageConfig.Redis) extends Recovery {
+  private val logger = LoggerFactory.getLogger(getClass)
+
   // Using Jedis for Redis operations
   private val jedis = {
+    logger.info(s"[RedisRecovery] Connecting to Redis at ${config.host}:${config.port}")
     val jedis = new Jedis(config.host, config.port)
     if (config.password.nonEmpty) {
       if (config.username.nonEmpty && config.username != "default") {
@@ -38,11 +43,14 @@ class RedisRecovery(config: StorageConfig.Redis) extends Recovery {
 
   override def recover(actorId: String): List[CborSerializable] = {
     import scala.jdk.CollectionConverters.*
+    logger.info(s"[RedisRecovery] Recovering events for actorId: $actorId from Redis")
     val pattern = s"recovery:$actorId:*"
     val keys    = jedis.keys(pattern).asScala.toList.sorted
     if (keys.isEmpty) {
+      logger.info(s"[RedisRecovery] No recovery events found for actorId: $actorId")
       List.empty[CborSerializable]
     } else {
+      logger.info(s"[RedisRecovery] Found ${keys.size} recovery events for actorId: $actorId")
       val list = jedis.mget(keys*).asScala.toList
       list.filter(_ != null).map(deserialize)
     }
@@ -54,14 +62,17 @@ class RedisRecovery(config: StorageConfig.Redis) extends Recovery {
     val keys    = jedis.keys(pattern)
     val nextId  = keys.size() + 1
     val newKey  = s"recovery:$actorId:$nextId"
+    logger.info(s"[RedisRecovery] Saving event for actorId: $actorId to Redis key: $newKey")
     jedis.set(newKey, serialize(event))
   }
 
   override def delete(actorId: String): Unit = {
     import scala.jdk.CollectionConverters.*
+    logger.info(s"[RedisRecovery] Deleting all recovery events for actorId: $actorId from Redis")
     val pattern = s"recovery:$actorId:*"
     val keys    = jedis.keys(pattern).asScala.toArray
     if (keys.nonEmpty) {
+      logger.debug(s"[RedisRecovery] Deleting keys: ${keys.mkString(", ")}")
       jedis.del(keys*)
     }
   }
